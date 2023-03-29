e9377370daad04e1346890e44630989e178a9a24
edna1996 <guantaiedna@gmapl.com>
2023-03-29 11:00:09 +0300

    Used fetch to get all user posts from db

--- src/pages/comments.js
+++ src/pages/comments.js
@@ -4,3 +4,12 @@ import { useParams } from 'react-router-dom';
 function Comments() {
     const { postId } = useParams();
     const [comments, setComments] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+          const response = await fetch(`https://example.com/posts/${postId}/comments`);
+          const data = await response.json();
+          setComments(data);
+        }
+        fetchData();
+      }, [postId]);
