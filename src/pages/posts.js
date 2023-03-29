92242edae97e80b4c6f17aadb443cca910e9c8f5
edna1996 <guantaiedna@gmapl.com>
2023-03-29 11:30:30 +0300

    Render posts dynamically and add links to author and comments pages

--- src/pages/posts.js
+++ src/pages/posts.js
@@ -4,3 +4,12 @@ import { Link } from 'react-router-dom';
 function Posts() {
     const [posts, setPosts] = useState([]);
   
+    useEffect(() => {
+        async function fetchData() {
+          const response = await fetch('https://example.com/posts');
+          const data = await response.json();
+          setPosts(data);
+        }
+        fetchData();
+      }, []);
+    
