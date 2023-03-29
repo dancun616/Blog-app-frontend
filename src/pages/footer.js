eeee0bd408f73bd6e515c85cf17847d1aadf5689
edna1996 <guantaiedna@gmapl.com>
2023-03-29 10:55:31 +0300

    updated the jsx data

--- src/pages/footer.js
+++ src/pages/footer.js
@@ -2,4 +2,21 @@ import React from 'react';
 
 const Footer = () => {
     const year = new Date().getFullYear()
+    
+    return (
+        <div>
+            <div>
+                <p>Copyright © {year}</p>
+                <p>
+                    Privacy Policy
+                </p>
+                <p>
+                    Terms and Conditions
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
 
