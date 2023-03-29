d2922ee9434977a868bceabda3d409bb033dccba
edna1996 <guantaiedna@gmapl.com>
2023-03-29 10:58:50 +0300

    created a function to allow user to add comments

--- src/pages/comments.js
+++ src/pages/comments.js
@@ -1,2 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+
+function Comments() {
+    const { postId } = useParams();
+    const [comments, setComments] = useState([]);
