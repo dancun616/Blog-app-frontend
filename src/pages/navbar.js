4a5a8c592c236faeabb574727cec229050fa7486
edna1996 <guantaiedna@gmapl.com>
2023-03-29 10:37:14 +0300

    created a function to display home page

--- src/pages/navbar.js
+++ src/pages/navbar.js
@@ -1,2 +1,9 @@
 import React from 'react';
 // import { NavLink, useNavigate } from 'react-router-dom'
+
+function NavBar({ setUser }) {
+    // const navigate = useNavigate()
+  
+    function onClickHome() {
+      navigate("/")
+    }
