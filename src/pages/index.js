43278e8d5bcb1f96c67a624dfae489cdbfd1201d
dancun616 <dancun616@gmail.com>
2023-03-29 09:50:03 +0300

    styled-sign-up

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,10 +4,10 @@ export default function Home() {
   return (
     <div className='home'>
       <h1 className='title'>Blog-post</h1>
-        <Link href="/login">Login</Link>
-        <br/>
-        <br/>
-        <Link href="/registration">SignUp</Link>
+      
+        <Link href="/login" className='login'>Login</Link>
+        
+        <Link href="/registration" className='signup' >SignUp</Link>
 
     </div>
   )
