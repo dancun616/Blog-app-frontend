dc2200420fb8c1c63dced2b3712773d203003da5
edna1996 <guantaiedna@gmapl.com>
2023-03-30 10:34:35 +0300

    Add state and form input handlers to Profile component

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,1 +1,7 @@
 import React, { useState } from 'react';
+
+function Profile() {
+    const [image, setImage] = useState('');
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [bio, setBio] = useState('');
